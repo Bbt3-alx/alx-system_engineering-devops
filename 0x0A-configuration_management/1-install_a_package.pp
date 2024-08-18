574035ef370f7288cc7cc4f7fcf858fcc677af58
Brehyma Traore <brehymatraore@gmail.com>
2024-08-18 19:43:18 +0000

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 
 # Ensure pip3 is installed
-package { 'python3-pip':
+package { 'python3-pip3':
   ensure => 'installed',
 }
 
@@ -10,5 +10,5 @@ package { 'python3-pip':
 # Install Flask using pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
