e6823b771b9aa54e797409c0997c1059e65159b6
Brehyma Traore <brehymatraore50@gmail.com>
2024-08-22 23:50:56 +0000

    Updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,14 @@
-# Increase the ulimite
+# Increase the ulimit for nginx
 
 exec { 'ulimit_nginx':
-  command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+  onlyif  => 'grep -q "15" /etc/default/nginx',
 }
 
+# Restart nginx to apply the new ulimit
 exec { 'restart_nginx':
   command => '/etc/init.d/nginx restart',
-  path    => '/usr/init.d/',
+  path    => '/etc/init.d/:/usr/local/sbin/:/usr/local/bin/:/usr/sbin/:/usr/bin/:/sbin/:/bin/',
+  require => Exec['ulimit_nginx'],
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,14 @@
 # login with the holberton user and open a file without any error message.
 
-exec { 'change_limit':
-  command => 'sed -i "/^holberton hard/s/4/50000" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+exec { 'change_hard_limit':
+  command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+  onlyif  => 'grep -q "^holberton hard.*4" /etc/security/limits.conf',
 }
 
-exec { 'change_limit':
-  command => 'sed -i "/^holberton soft/s/5/50000" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+exec { 'change_soft_limit':
+  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+  onlyif  => 'grep -q "^holberton soft.*5" /etc/security/limits.conf',
+  require => Exec['change_hard_limit'],
 }
